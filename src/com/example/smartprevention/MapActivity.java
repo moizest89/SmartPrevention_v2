09f8325be875a5b1be0c2cb60f4c42703a3e4739
Moises Portillo <moises.gilberto@gmail.com>
2013-10-10 01:16:35 -0600

    update

---
+++ src/com/example/smartprevention/MapActivity.java
@@ -0,0 +1,62 @@
+package com.example.smartprevention;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MapActivity extends Activity {
+
+    private GoogleMap googleMap;
+    static final LatLng ESA = new LatLng(-89.63., 13.87);
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+ 
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+ 
+    public void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "No se pudo crear el mapa", Toast.LENGTH_SHORT)
+                        .show();
+            }else{
+            	//googleMap.setMyLocationEnabled(true);
+            	//googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            	googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ESA, 13));
+            }
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
